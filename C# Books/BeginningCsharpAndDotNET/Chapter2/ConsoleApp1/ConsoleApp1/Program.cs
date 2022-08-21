cadd7597b2b5a3d371134ab0444bf920b56a9843
JanHappy <yan.kozhyn@gmail.com>
2022-08-21 13:12:14 +0300

    Add project files.

---
+++ C# Books/BeginningCsharpAndDotNET/Chapter2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The first app in Beginning C# and .NET!");
+            Console.ReadKey();
+        }
+    }
+}
