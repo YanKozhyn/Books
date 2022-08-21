121b529df618e4a7086fba7b733f6faca20f1b64
JanHappy <yan.kozhyn@gmail.com>
2022-08-21 22:25:21 +0300

    end chapter 4

---
+++ C# Books/BeginningCsharpAndDotNET/Chapter4/Ch04Ex01/Ch04Ex01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using static System.Convert;
+
+namespace Ch04Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter an integer");
+            int myInt = ToInt32(ReadLine());
+            bool isLessThan10 = myInt < 10;
+            bool isBetween0And5 = (0 <= myInt) && (myInt <= 5);
+            WriteLine($"Integer less then 10? {isLessThan10}");
+            WriteLine($"Integer between 0 and 5? {isBetween0And5}");
+            WriteLine($"Exactly one of the above is true?" +
+                $"{isLessThan10 ^ isBetween0And5}");
+
+            //<test> ? <resultIfTrue>: <resultIfFalse>;
+            string resultString = (myInt < 10) ? "Less than 10"
+                                            : "greater than or equal to 10";
+            WriteLine(resultString);
+        }
+
+    }
+
+}
---
+++ C# Books/BeginningCsharpAndDotNET/Chapter4/HomeWork/HomeWork/Program.cs
@@ -0,0 +1,56 @@
+﻿/*4.1 If you have two integers stored in variables var1 and var2, what Boolean test can you perform
+to determine whether one or the other (but not both) is greater than 10 ?*/
+///Answer: (var1 > 10) ^ (var2 > 10)
+
+
+/*4.2 What is wrong with the following code?
+int i;
+for (i = 1; i <= 10; i++)
+{
+ if ((i % 2) = 0)
+ continue;
+ Console.WriteLine(i);
+} */
+
+///Answer: Using the = assignment operator instead of the Boolean == operator is a very common mistake.
+
+
+/* 4.3 Write an application that includes the logic from Exercise 1, obtains two numbers from the user,
+and displays them, but rejects any input where both numbers are greater than 10 and asks for
+two new numbers.*/
+///Answer: 
+
+using static System.Console;
+using static System.Convert;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool numbersOK = false;
+            double var1, var2;
+            var1 = 0;
+            var2 = 0;
+            while (!numbersOK)
+            {
+                WriteLine("Enter 2 numbers, both numbers cannot be greater than 10.");
+                Write("Please enter the first number: ");
+                var1 = ToDouble(ReadLine());
+                Write("Please enter the second number: ");
+                var2 = ToDouble(ReadLine());
+                WriteLine($"The first number entered is {var1} " +
+                    $"and the second is {var2}");
+                if ((var1 > 10) ^ (var2 > 10))
+                    numbersOK = true;
+                else
+                    WriteLine("Only one number may be greater than 10, " +
+                            "please try again.");
+            }
+            WriteLine("Press the <ENTER> key to exit.");
+            ReadLine();
+        }
+    }
+}
+
